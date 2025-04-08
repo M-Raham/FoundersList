bd260570e7c1f1e4e2b9d1de2cefb8ea8096649b
Muhammad Raham <rahamm711@gmail.com>
2025-04-08 06:49:36 -0700

    Enhance Home component with updated heading and sub-heading, and refine CSS formatting

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <>
-      <section className="pink_container">
-        <h1 className="heading">Home</h1>
+      <section className="pink_container pattern">
+        <h1 className="heading">Pitch your startup, <br /> connect with enterpreneurs</h1>
+        <p className="sub-heading !max-w-3xl">Submit ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
       </section>
     </>
   );
