dbc2d4099dc19374887831ed97e5a8b8c3dd6ba3
Muhammad Raham <rahamm711@gmail.com>
2025-04-09 03:38:49 -0700

    Refactor layout and page components, update Navbar and SearchForm, and add button component with Radix UI integration

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container pattern">
-        <h1 className="heading">Pitch your startup, <br /> connect with enterpreneurs</h1>
-        <p className="sub-heading !max-w-3xl">Submit ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
+        <h1 className="heading">
+          Pitch your startup, <br /> connect with enterpreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
-} 
+}
