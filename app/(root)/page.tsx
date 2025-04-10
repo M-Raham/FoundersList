9c87f5e28609e4b3c74149bd219d79d3f2f57d15
Muhammad Raham <rahamm711@gmail.com>
2025-04-10 00:28:23 -0700

    fix: update author name in the sample post data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,7 +13,7 @@ export default async function Home({
       _createdAT: new Date(),
       _id: "1",
       views: 100,
-      author: { _id: 1, name: "Raham"},
+      author: { _id: 1, name: "John"},
       description: "This is a test description",
       image:
         "https://plus.unsplash.com/premium_vector-1728572089769-392cad45b580?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
