b4dadd6d5d6aa9696040f73750bdc410c18ed911
Muhammad Raham <rahamm711@gmail.com>
2025-04-10 03:29:42 -0700

    feat: integrate Sanity client and update Home component to fetch posts dynamically

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
   searchParams,
@@ -8,19 +10,7 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
-  const posts = [
-    {
-      _createdAT: new Date(),
-      _id: "1",
-      views: 100,
-      author: { _id: 1, name: "John"},
-      description: "This is a test description",
-      image:
-        "https://plus.unsplash.com/premium_vector-1728572089769-392cad45b580?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Robots",
-      title: "We Robots",
-    },
-  ];
+  const posts = await client.fetch(STARTUP_QUERY);
 
   return (
     <>
@@ -42,7 +32,9 @@ export default async function Home({
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType, number) => (<StartupCard key={post?._id} post={post} />))
+            posts.map((post: StartupCardType, number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
           ) : (
             <p className="no-result">No startups found</p>
           )}
