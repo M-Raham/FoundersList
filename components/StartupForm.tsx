262cf72f8fd25a3c56df23d3b7c6b9dbc33307a3
Muhammad Raham <rahamm711@gmail.com>
2025-04-23 23:56:08 -0700

    feat: refactor StartupForm component structure and add new dependencies

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,9 +1,18 @@
-"use client"
+"use client";
+
+import { Input } from "./ui/input";
 
 const StartupForm = () => {
   return (
-    <form action={() => {}} className='startup-form'>Startupform</form>
-  )
-}
+    <form action={() => {}} className="startup-form">
+      <div>
+        <label htmlFor="title" className="startup-form_label">
+          Title
+        </label>
+        <Input id="title" name="title" className="startup-form_input" required placeholder="Startup Title" />
+      </div>
+    </form>
+  );
+};
 
-export default StartupForm
+export default StartupForm;
