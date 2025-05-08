12e40635905776192a70d3460492c19a06babbf9
Muhammad Raham <rahamm711@gmail.com>
2025-05-08 06:49:07 -0700

    feat: enhance user profile page with image display and user details

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 import { client } from "@/sanity/lib/client";
 import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -15,9 +16,31 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
         <div className="profile_card">
           <div className="profile_title">
             <h3 className="text-24-black uppercase text-center line-clamp-1">
-                {user.name}
+              {user.name}
             </h3>
           </div>
+
+          <Image
+            src={user.image}
+            alt={user.name}
+            width={220}
+            height={220}
+            className="profile_image"
+          />
+
+          <p className="text-30-extrabold mt-7 text-center">
+            @{user?.username}
+          </p>
+          <p className="text-14-normal mt-1 text-center">@{user?.bio}</p>
+        </div>
+
+        <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
+          <p className="text-30-bold">
+            {session?.id === id ? "Your" : "All"} Startups  
+          </p>
+          <ul className="card_grid-sm">
+
+          </ul>
         </div>
       </section>
     </>
