5fd12ad9fabf96c1d5b9758b087ac8ef65b499a1
Muhammad Raham <rahamm711@gmail.com>
2025-05-09 00:25:30 -0700

    feat: add playlist schema to Sanity and update Navbar logo dimensions

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async () => {
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <img src="/logo.png" alt="logo" width={144} height={30} />
+          <img src="/logo.png" alt="logo" width={165} height={30} />
         </Link>
 
         <div className="flex items-center gap-5 text-black">
